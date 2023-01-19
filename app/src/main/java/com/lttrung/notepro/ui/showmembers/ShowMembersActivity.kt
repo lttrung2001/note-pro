d096fe1fe17834d83994e5e766be12901294bcc9
Le Thanh Trung <lt.trung2001@gmail.com>
2023-01-19 22:45:24 +0700

    add add member button ui

--- app/src/main/java/com/lttrung/notepro/ui/showmembers/ShowMembersActivity.kt
+++ app/src/main/java/com/lttrung/notepro/ui/showmembers/ShowMembersActivity.kt
@@ -1,9 +1,11 @@
 package com.lttrung.notepro.ui.showmembers
 
 import android.os.Bundle
+import android.view.Menu
 import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.WindowCompat
+import com.lttrung.notepro.R
 import com.lttrung.notepro.databinding.ActivityShowMembersBinding
 
 class ShowMembersActivity : AppCompatActivity() {
@@ -21,5 +23,18 @@ class ShowMembersActivity : AppCompatActivity() {
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
         onBackPressed()
         return super.onOptionsItemSelected(item)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu_show_members, menu)
+        return true
+    }
+
+    override fun onContextItemSelected(item: MenuItem): Boolean {
+        if (item.itemId == R.id.action_add_member) {
+            return true
+        } else {
+            return super.onContextItemSelected(item)
+        }
     }
 }
